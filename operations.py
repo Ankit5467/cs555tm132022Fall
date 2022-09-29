a156095d609e01f65180b6c225cdd21521b1fed5
Ankit Patel <ankit.patel.1701.5467@gmail.com>
2022-09-29 16:29:34 -0400

    Implemented User Story #2

--- operations.py
+++ operations.py
@@ -1,6 +1,12 @@
 # Define feature functions (user story functions) and variables in this file:
 
 from datetime import date, datetime
+
+# Import helper functions
+from helpers import convertDateStrToDateTuple
+
+# Import constants 
+from helpers import DAY_IND, MONTH_IND, YEAR_IND
  
 # familes = []
 # individuals = []
@@ -15,10 +21,38 @@ def getNameFromId(id, people):
     return 'Error: Id does not exist!'
 
 
+# User Story #2 -- Ankit
+# Input: A person object/dictionary
+# Output: Returns true if birth occurs before death on an individual. False otherwise.
+# Note: Returns true if person is not dead. 
+# Question: Can someone die on the same day they were born? This function assumes thats allowed
+def birthBeforeDeath(personObj):
+    if personObj['alive']:
+        return True
+    else:
+        birthdayTuple = convertDateStrToDateTuple(personObj['birthday'])
+        deathdayTuple = convertDateStrToDateTuple(personObj['death'])
+        
+        if birthdayTuple[YEAR_IND] > deathdayTuple[YEAR_IND]:
+            return False
+        elif birthdayTuple[YEAR_IND] < deathdayTuple[YEAR_IND]:
+            return True
+        else:
+            # Check month:
+            if birthdayTuple[MONTH_IND] < deathdayTuple[MONTH_IND]:
+                return True
+            elif birthdayTuple[MONTH_IND] > deathdayTuple[MONTH_IND]:
+                return False
+            else:
+                # Check day:
+                return birthdayTuple[DAY_IND] <= deathdayTuple[DAY_IND]
+    
+
 # User Story #27 -- Ankit
 # Input: a person object/dictionary
 # Output: Computes the age of the person
 # Note: DOES modify the input- slightly formats the person object to make date extraction easier for future uses.
+# Question: SHould the program accept future dates?
 def computeAge(person):       
     bdayLen = len(person['birthday'].split(' ')[0])
     if bdayLen == 1:
